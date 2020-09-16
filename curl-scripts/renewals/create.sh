2dc76fd1f4b7dd2cbe115ef5f1fa434aaa276c60
MelNesbitt12 <mel.nalband@gmail.com>
2020-09-16 09:35:52 -0400

    Updated curl scripts for resource

---
+++ curl-scripts/renewals/create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/renewals"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+--header "Authorization: Bearer ${TOKEN}" \
+--data '{
+    "renewal": {
+      "name": "'"${NAME}"'",
+      "type": "'"${TYPE}"'",
+      "date": "'"${DATE}"'",
+      "url": "'"${URL}"'",
+      "comments": "'"${COMMENTS}"'"
+    }
+  }'
+
+echo
