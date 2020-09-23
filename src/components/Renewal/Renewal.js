2b081fc26cc7c4ff0dd321ae058b3e79eb9b3644
MelNesbitt12 <mel.nalband@gmail.com>
2020-09-23 11:55:47 -0400

    Styling edits to layout of indexes Renewals
    minor styling changes in renewal.js, renewalform.js, renewals.js
    
    Fixed footer so that it stays fixed to bottom of scrolling page in
    index.html and index.scss

--- src/components/Renewal/Renewal.js
+++ src/components/Renewal/Renewal.js
@@ -112,13 +112,13 @@ class Renewal extends Component {
         <Card className="show" style={{ width: '30rem', margin: 'auto', textAlign: 'center' }}>
           <img className="card-img-top" src="images.png" style={{ width: '10rem' }}></img>
           <Card.Body>
-            <Card.Title style={{ textAlign: 'center', fontSize: '25px' }}>{renewal.name}</Card.Title><br/>
-            <p>Type: {renewal.type}</p>
-            <p>Expiration: {getFullDate}</p>
-            <p>Days Until Expiration: {renewal.needsRenew}</p>
-            <p>Cost to Renew: ${renewal.cost}</p>
-            <a href={renewal.url} rel="noopener noreferrer" target="_blank"> Link to Renew </a>
-            <p>Instructions: {renewal.instructions}</p>
+            <Card.Title style={{ textAlign: 'center', fontSize: '25px', fontWeight: 'bold', textShadow: '1px 1px' }}>{renewal.name}</Card.Title><br/>
+            <p style={{ fontSize: '20px' }}>Type: {renewal.type}</p>
+            <p style={{ fontSize: '20px' }}>Expiration: {getFullDate}</p>
+            <p style={{ fontSize: '20px' }}>Days Until Expiration: {renewal.needsRenew}</p>
+            <p style={{ fontSize: '20px' }}>Cost to Renew: ${renewal.cost}</p>
+            <a href={renewal.url} rel="noopener noreferrer" target="_blank" style={{ fontSize: '20px' }}> Link to Renew </a>
+            <p style={{ fontSize: '20px' }}>Instructions: {renewal.instructions}</p>
             <Button className="submit-btn" variant="outline-info" onClick={this.handleClick}>Update</Button>
             <Button className="submit-btn" variant="outline-info" onClick={this.deleteRenewal}>Delete</Button><br/>
             <p></p><br/>
--- src/components/Renewal/Renewals.js
+++ src/components/Renewal/Renewals.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import CardDeck from 'react-bootstrap/CardDeck'
 import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Nav from 'react-bootstrap/Nav'
 
 import apiUrl from '../../apiConfig'
 import messages from '../AutoDismissAlert/messages'
@@ -68,34 +71,60 @@ class Renewals extends Component {
       const getFullDate = month + '/' + day + '/' + year
 
       return (
-        <div key={renewal._id}>
-          <Card className="card" style={{ width: '18rem', margin: 'auto', textAlign: 'center', fontSize: '20px' }} >
+        <Col sm='4' key={renewal._id}>
+          <Card className="card" style={{ width: '18rem', margin: '10px', textAlign: 'center', fontSize: '20px', padding: '10px', borderColor: '#459342' }} >
+            <Card.Header>
+              <Nav variant="tabs" defaultActiveKey="#first">
+                <Nav.Item style={{ height: '2rem' }}>
+                  <Link to={`/renewals/${renewal._id}`} style={{ alt: 'Open' }}> 📝 </Link>
+                </Nav.Item>
+              </Nav>
+            </Card.Header>
             <Card.Body>
-              <Link to={`/renewals/${renewal._id}`}>
-                {renewal.name}
-              </Link><br/>
-              Expiration: {getFullDate}
+              <Card.Title style={{ fontSize: '25px' }}>{renewal.name}</Card.Title>
+              <Card.Text sytle={{ fontSize: '25px' }}>
+                Expiration: {getFullDate}
+              </Card.Text>
             </Card.Body>
           </Card>
-        </div>
+        </Col>
       )
+
+      //   <div key={renewal._id}>
+      //     <Card className="card" style={{ width: '18rem', margin: 'auto', textAlign: 'center', fontSize: '20px' }} >
+      //       <Card.Body>
+      //         <Link to={`/renewals/${renewal._id}`}>
+      //           {renewal.name}
+      //         </Link><br/>
+      //         Expiration: {getFullDate}
+      //       </Card.Body>
+      //     </Card>
+      //   </div>
+      // )
     })
 
     return (
-      <div className="renewals">
-        <CardDeck className="scroll" style={{ width: '30rem', margin: 'auto' }}>
-          <Card style={{ width: '30rem', margin: 'auto', borderRadius: '35px', padding: '10px', borderColor: '#459342' }}>
-            <Card.Body>
-              <Card.Title style={{ textAlign: 'center' }}>Renewals</Card.Title><br/>
-              {renewals}
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Perennial Renewal Tracker</small>
-            </Card.Footer>
-          </Card>
-        </CardDeck>
-      </div>
+      <CardDeck className="renewals scroll">
+        <h1 className="renewal-title" style={{ fontWeight: 'bold', textAlign: 'center', textShadow: '1px 1px', margin: 'auto' }}>Renewals</h1>
+        <Row>
+          {renewals}
+        </Row>
+      </CardDeck>
     )
+    //   <div className="renewals">
+    //     <CardDeck className="scroll" style={{ width: '30rem', margin: 'auto' }}>
+    //       <Card style={{ width: '30rem', margin: 'auto', borderRadius: '35px', padding: '10px', borderColor: '#459342' }}>
+    //         <Card.Body>
+    //           <Card.Title style={{ textAlign: 'center' }}>Renewals</Card.Title><br/>
+    //           {renewals}
+    //         </Card.Body>
+    //         <Card.Footer>
+    //           <small className="text-muted">Perennial Renewal Tracker</small>
+    //         </Card.Footer>
+    //       </Card>
+    //     </CardDeck>
+    //   </div>
+    // )
   }
 }
 
